545f47018786e776dd3722e4b4b0b37372fb3fed
Biophilia <127826463+BiophiliaSWDA@users.noreply.github.com>
2023-11-15 19:38:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,10 +8,16 @@ from fib import Fib
 class TestGetNumber(unittest.TestCase):
 
   def test_ok(self):
-    self.assertEquals(GetNumber(), 42)
-
+    if GetNumber() == 42:
+    	print("Number == ", GetNumber())
+    else:
+    	print("Number != ", GetNumber())
+    	
   def test_fib(self):
-    self.assertEquals(Fib(5), 8)
+    if Fib(5) == 8:
+    	print("Fib(5) == ", Fib(5))
+    else:
+    	print("Fib(5) != ", Fib(5))
 
 if __name__ == '__main__':
   unittest.main()
